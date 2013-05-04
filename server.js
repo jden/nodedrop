cb5aaef7ad5aacb0178cb3f6902de87e3ed188ca
jden <jason@denizac.org>
2013-05-04 15:26:27 -0700

    basic socket functionality

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express')
+var http = require('http')
 var sio = require('socket.io')
 var app = express()
+
+var server = http.createServer(app)
 
 
 app.post('/upload', function () {
 
-
-
+  console.log('uploaded')
 
 })
 
@@ -14,12 +16,31 @@ app.post('/upload', function () {
 app.get('/*', express.static(__dirname + '/public'))
 
 
+io = sio.listen(server)
 
-io = sio.listen(app)
+var peers = {}
 
 io.sockets.on('connection', function (socket) {
-  socket.broadcast.emit('enter')
+  peers[socket.id] = {name: socket.id}
+  socket.emit('roster', peers)
+  socket.broadcast.emit('enter', socket.id)
+
+  socket.on('disconnect', function () {
+    delete peers[socket.id]
+    socket.broadcast.emit('exit', socket.id)
+  })
+
+  socket.on('namechange', function (name) {
+    console.log('namechange', socket.id, name)
+    socket.broadcast.emit('namechange', socket.id, name)
+  })
+
+
 })
 
 
-app.listen(process.env.PORT || 2323)
+
+
+var port = process.env.PORT || 2323
+server.listen(port)
+console.log('server started on ' + port)
